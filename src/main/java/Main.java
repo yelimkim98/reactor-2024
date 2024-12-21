6d4158c01feb766302d8dbbeaece524e68a4b5fa
yerimkim <kiel0103@naver.com>
2024-12-21 18:03:44 +0900

    예제 7-2 Hot Sequence

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
+import java.time.Duration;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Flux<String> flux = Mono.justOrEmpty("Steve")
-        .concatWith(Mono.justOrEmpty("Jobs"));
+  public static void main(String[] args) throws InterruptedException {
+    String[] singers = {"A", "B", "C", "D", "E"};
 
-    flux.subscribe(System.out::println);
+    System.out.println("# begin concert:");
+
+    Flux<String> concertFlux =
+        Flux
+            .fromArray(singers)
+            .delayElements(Duration.ofSeconds(1))
+            .share();
+
+    concertFlux.subscribe(
+        singer -> System.out.println(String.format("# Subscriber1 is watching %s's song", singer))
+    );
+    Thread.sleep(2500);
+
+    concertFlux.subscribe(
+        singer -> System.out.println(String.format("# Subscriber2 is watching %s's song", singer))
+    );
+    Thread.sleep(3000);
   }
 }
