a247948d6931046a361b00175be06c2231a343a2
yerimkim <kiel0103@naver.com>
2024-12-21 15:22:27 +0900

    예제 6-2 Mono.empty()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class Main {
 
   public static void main(String[] args) {
-    Flux<String> sequence = Flux.just("Hello", "Reactor");
-
-    sequence.map(String::toLowerCase)
-        .subscribe(System.out::println);
+    Mono.empty()
+        .subscribe(
+            none -> {},
+            error -> {},
+            () -> System.out.println("onComplete")
+        );
   }
 }
