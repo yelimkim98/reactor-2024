76b7483633bb3c55b2da57871c5070b72a91b6e7
yerimkim <kiel0103@naver.com>
2024-12-21 16:10:59 +0900

    예제 6-5 Flux

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,11 @@
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import java.net.URI;
-import java.util.Collections;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public class Main {
 
   public static void main(String[] args) {
-    URI uri = UriComponentsBuilder.newInstance()
-        .scheme("http")
-        .host("worldtimeapi.org")
-        .port(80)
-        .path("api/timezone/Asia/Seoul")
-        .build()
-        .encode()
-        .toUri();
-
-    RestTemplate restTemplate = new RestTemplate();
-
-    HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-    Mono.just(restTemplate.exchange(
-            uri, HttpMethod.GET, new HttpEntity<String>(httpHeaders), String.class
-        ))
-        .map(response -> {
-          DocumentContext jsonContext = JsonPath.parse(response.getBody());
-          return jsonContext.<String>read("$.datetime");
-        })
-        .subscribe(
-            data -> System.out.println("# emitted data: " + data),
-            System.out::println,
-            () -> System.out.println("# emitted onComplete signal")
-        );
+    Flux.fromArray(new Integer[]{3, 6, 7, 9,})
+        .filter(num -> num > 6)
+        .map(num -> num * 2)
+        .subscribe(System.out::println);
   }
 }
