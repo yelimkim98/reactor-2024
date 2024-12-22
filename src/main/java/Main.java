a3c4f7b85d8d24ccf5827ebfd07931ab18524ab7
yerimkim <kiel0103@naver.com>
2024-12-22 17:33:20 +0900

    예제 8-2 Backpressure ERROR 전략

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,24 @@
-import lombok.SneakyThrows;
-import org.reactivestreams.Subscription;
-import reactor.core.publisher.BaseSubscriber;
+import java.time.Duration;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Flux.range(1, 5)
-        .doOnRequest(data -> System.out.printf("# doOnRequest: %s\n", data))
-        .subscribe(new BaseSubscriber<Integer>() {
+  public static void main(String[] args) throws InterruptedException {
+    Flux.interval(Duration.ofMillis(1L))    // Publisher : 0.001 초에 한번씩 emit (계속 반복)
+        .onBackpressureError()    // Backpressure 전략 중 ERROR 전략 사용
+        .doOnNext(data -> System.out.printf("# doOnNext: %s\n", data))  // 걍 디버깅용 이라고 함
+        .publishOn(Schedulers.parallel())    // ?
+        .subscribe(data -> {
+              try {
+                Thread.sleep(5L);    // Subscriber 가 데이터 처리하는데 0.005 초 걸리는 시나리오
+              } catch (InterruptedException e) {
+              }
 
-          /*
-           구독 시점에 실행됨
-           */
-          @Override
-          protected void hookOnSubscribe(Subscription subscription) {
-            request(1);  // subscription.request(1) 호출
-          }
+              System.out.printf("# onNext : %s\n", data);
+            },
+            error -> System.out.println("# onError"));
 
-          /*
-
-           */
-          @SneakyThrows    // ?
-          @Override
-          protected void hookOnNext(Integer value) {
-            Thread.sleep(2000L);
-            System.out.printf("# hookOnNext: %s\n", value);
-            request(1);    // // subscription.request(1) 호출
-          }
-        });
+    Thread.sleep(2000L);
   }
 }
