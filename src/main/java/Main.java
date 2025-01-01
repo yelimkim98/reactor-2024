c3bf64fb9349f6b53c39744d5c30a0dbaa3b44fa
yerimkim <kiel0103@naver.com>
2025-01-01 21:41:23 +0900

    예제 9-2 Sinks 사용 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.util.stream.IntStream;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Sinks;
+import reactor.core.publisher.Sinks.EmitFailureHandler;
 import reactor.core.scheduler.Schedulers;
 
 public class Main {
@@ -8,16 +9,36 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
     int tasks = 6;
 
-    Flux.create((FluxSink<String> sink) -> IntStream.range(1, tasks)
-        .forEach(n -> sink.next("task " + n + " result")))
-        .subscribeOn(Schedulers.boundedElastic())
-        .doOnNext(n -> System.out.printf("### [%s] create(): %s\n", Thread.currentThread().getName(),n))
+    Sinks.Many<String> unicastSink = Sinks.many().unicast().onBackpressureBuffer();
+    Flux<String> fluxView = unicastSink.asFlux();
+    IntStream
+        .range(1, tasks)
+        .forEach(n -> {
+          try {
+            new Thread(() -> {
+              unicastSink.emitNext(doTask(n), EmitFailureHandler.FAIL_FAST);
+              System.out.printf("## [%s] emitted: %s\n", Thread.currentThread().getName(), n);
+            }).start();
+            Thread.sleep(100L);
+          } catch (InterruptedException e) {
+            System.out.printf("## [%s] %s\n", Thread.currentThread().getName(), e.getMessage());
+          }
+        });
+    fluxView
         .publishOn(Schedulers.parallel())
         .map(result -> result + " success!")
-        .doOnNext(n -> System.out.printf("### [%s] map(): %s\n", Thread.currentThread().getName(),n))
+        .doOnNext(
+            n -> System.out.printf("## [%s] map(): %s\n", Thread.currentThread().getName(), n))
         .publishOn(Schedulers.parallel())
-        .subscribe(data -> System.out.printf("### [%s] onNext: %s\n", Thread.currentThread().getName(), data));
+        .subscribe(
+            data -> System.out.printf("## [%s] onNext: %s\n", Thread.currentThread().getName(), data));
 
-    Thread.sleep(500L);
+    Thread.sleep(200L);
+  }
+
+  private static String doTask(int taskNumber) {
+    // now tasking
+    // complete to task
+    return "task " + taskNumber + " result";
   }
 }
