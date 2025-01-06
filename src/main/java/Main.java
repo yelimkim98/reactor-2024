013280a3d65bfd03c786bb21a55f97696ae71a39
yerimkim <kiel0103@naver.com>
2025-01-06 20:14:03 +0900

    예제 10-5 publishOn과 subscribeOn을 사용하지 않았을 경우

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,23 @@
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.Sinks;
-import reactor.core.publisher.Sinks.EmitFailureHandler;
+import reactor.core.publisher.Flux;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
-    Sinks.One<String> sinkOne = Sinks.one();
-    Mono<String> mono = sinkOne.asMono();
-
-    sinkOne.emitValue("Hello Reactor", EmitFailureHandler.FAIL_FAST);
-    sinkOne.emitValue("Hi Reactor", EmitFailureHandler.FAIL_FAST);
-
-    mono.subscribe(data -> System.out.printf("## [%s] Subscriber1 %s\n", Thread.currentThread().getName(), data));
-    mono.subscribe(data -> System.out.printf("## [%s] Subscriber2 %s\n", Thread.currentThread().getName(), data));
-
-
+  /*
+   * 항상 똑같은 순서로 처리됨
+   *
+   * Reactor 의 기본 동작은
+   * 호출한 스레드(main 스레드)에서 동기적으로 데이터 스트림을 처리하는 것입니다.
+   *
+   * 별도의 비동기 처리(ex: Schedulers)를 명시적으로 추가하지 않았기 때문에, 모든 연산은 순차적으로 실행됩니다.
+   */
+  public static void main(String[] args) {
+    Flux
+        .fromArray(new Integer[] {1, 3, 5, 7})
+        .doOnNext(data -> System.out.printf("# [%s] doOnNext fromArray: %s\n", Thread.currentThread().getName(), data))
+        .filter(data -> data > 3)
+        .doOnNext(data -> System.out.printf("# [%s] doOnNext filter: %s\n", Thread.currentThread().getName(), data))
+        .map(data -> data * 10)
+        .doOnNext(data -> System.out.printf("# [%s] doOnNext map: %s\n", Thread.currentThread().getName(), data))
+        .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
   }
 }
