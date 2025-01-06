448d0b174067d43a620e88dde7c2cc8beb5cc67f
yerimkim <kiel0103@naver.com>
2025-01-06 22:06:13 +0900

    예제 11-6 Context 특징 예제 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,20 @@ public class Main {
 
   public static void main(String[] args) throws InterruptedException {
     String key1 = "company";
+    String key2 = "name";
 
-    Mono<String> mono = Mono.deferContextual(
-            contextView -> Mono.just("Company: " + " " + contextView.get(key1)))
-        .publishOn(Schedulers.parallel());
-
-    mono.contextWrite(context -> context.put(key1, "Apple"))
-        .subscribe(data -> System.out.printf("# [%s] subscribe1 onNext: %s\n", Thread.currentThread().getName(), data));
-
-    mono.contextWrite(context -> context.put(key1, "Microsoft"))
-        .subscribe(data -> System.out.printf("# [%s] subscribe2 onNext: %s\n", Thread.currentThread().getName(), data));
-
-
+    Mono
+        .deferContextual(contextView ->
+            Mono.just(contextView.get(key1))
+        )
+        .publishOn(Schedulers.parallel())
+        .contextWrite(context -> context.put(key2, "Bill"))
+        .transformDeferredContextual((mono, context) ->
+            // context 에 key2 가 없다고 됨
+            mono.map(data -> data + ", " + context.getOrDefault(key2, "Steve"))
+        )
+        .contextWrite(context -> context.put(key1, "Apple"))
+        .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
 
     Thread.sleep(100L);
   }
