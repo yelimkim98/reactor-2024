a5f0f77a1bc254bec26b2be62af08de067ae231b
yerimkim <kiel0103@naver.com>
2025-01-06 21:46:33 +0900

    예제 11-5 Context 특징 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,22 @@
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-import reactor.util.context.Context;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
     String key1 = "company";
-    String key2 = "firstname";
-    String key3 = "lastname";
 
-    Mono.deferContextual(ctx ->
-            Mono.just(ctx.get(key1) + ", " + ctx.get(key2) + ", " + ctx.get(key3))
-        )
-        .publishOn(Schedulers.parallel())
-        .contextWrite(context ->
-            context.putAll(Context.of(key2, "Steve", key3, "Jobs").readOnly())
-        )
-        .contextWrite(context -> context.put(key1, "Apple"))
-        .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
+    Mono<String> mono = Mono.deferContextual(
+            contextView -> Mono.just("Company: " + " " + contextView.get(key1)))
+        .publishOn(Schedulers.parallel());
+
+    mono.contextWrite(context -> context.put(key1, "Apple"))
+        .subscribe(data -> System.out.printf("# [%s] subscribe1 onNext: %s\n", Thread.currentThread().getName(), data));
+
+    mono.contextWrite(context -> context.put(key1, "Microsoft"))
+        .subscribe(data -> System.out.printf("# [%s] subscribe2 onNext: %s\n", Thread.currentThread().getName(), data));
+
+
 
     Thread.sleep(100L);
   }
