9b7ec9491d288391fd8c4c51196f8ab7bd786952
yerimkim <kiel0103@naver.com>
2025-01-06 22:28:44 +0900

    예제 11-8 Context 특징 예제 - 인증

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,38 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
-    String key1 = "company";
-    String key2 = "name";
+  private static final String HEADER_AUTH_TOKEN = "authToken";
 
-    Mono
-        .deferContextual(contextView ->
-            Mono.just(contextView.get(key1))
-        )
-        .publishOn(Schedulers.parallel())
-        .contextWrite(context -> context.put(key2, "Bill"))
-        .transformDeferredContextual((mono, context) ->
-            // context 에 key2 가 없다고 됨
-            mono.map(data -> data + ", " + context.getOrDefault(key2, "Steve"))
-        )
-        .contextWrite(context -> context.put(key1, "Apple"))
-        .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
+  public static void main(String[] args) {
+    Mono<String> mono = postBook(Mono.just(new Book("abdc-1111-3533-2809", "Reactor's Bible", "Kevin")))
+        .contextWrite(Context.of(HEADER_AUTH_TOKEN, "eyJhbGciOi"));
 
-    Thread.sleep(100L);
+    mono.subscribe(data -> System.out.printf("# [%s] onNext: %s", Thread.currentThread().getName(), data));
   }
+
+  private static Mono<String> postBook(Mono<Book> book) {
+    return Mono.zip(
+            book,
+            Mono.deferContextual(contextView -> Mono.just(contextView.get(HEADER_AUTH_TOKEN)))
+        )
+        .flatMap(tuple -> {
+          String response =
+              "POST the book(" + tuple.getT1().getBookName() + "," + tuple.getT1().getAuthor()
+                  + ") with token: " + tuple.getT2();
+          return Mono.just(response);  // HTTP POST 전송을 했다고 가정
+        });
+  }
+}
+
+@AllArgsConstructor
+@Data
+class Book {
+
+  private String isbn;
+  private String bookName;
+  private String author;
 }
