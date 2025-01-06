2f10799455a3c789f075e89f7fea12ff8c732f1f
yerimkim <kiel0103@naver.com>
2025-01-06 21:37:36 +0900

    예제 11-3 Context, ContextView

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,24 @@
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
 
 public class Main {
 
-  /*
-   * 항상 똑같은 순서로 처리됨
-   *
-   * Reactor 의 기본 동작은
-   * 호출한 스레드(main 스레드)에서 동기적으로 데이터 스트림을 처리하는 것입니다.
-   *
-   * 별도의 비동기 처리(ex: Schedulers)를 명시적으로 추가하지 않았기 때문에, 모든 연산은 순차적으로 실행됩니다.
-   */
   public static void main(String[] args) throws InterruptedException {
-    doTask("task1")
+    String key1 = "company";
+    String key2 = "firstname";
+    String key3 = "lastname";
+
+    Mono.deferContextual(ctx ->
+            Mono.just(ctx.get(key1) + ", " + ctx.get(key2) + ", " + ctx.get(key3))
+        )
+        .publishOn(Schedulers.parallel())
+        .contextWrite(context ->
+            context.putAll(Context.of(key2, "Steve", key3, "Jobs").readOnly())
+        )
+        .contextWrite(context -> context.put(key1, "Apple"))
         .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
 
-    doTask("task2")
-        .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
-
-    Thread.sleep(200L);
-  }
-
-  private static Flux<Integer> doTask(String taskName) {
-    return Flux.fromArray(new Integer[]{1, 3, 5, 7})
-        .publishOn(Schedulers.single())
-        .filter(data -> data > 3)
-        .doOnNext(data -> System.out.printf("# [%s] %s doOnNext filter: %s\n", Thread.currentThread().getName(), taskName, data))
-        .map(data -> data * 10)
-        .doOnNext(data -> System.out.printf("# [%s] %s doOnNext map: %s\n", Thread.currentThread().getName(), taskName, data));
+    Thread.sleep(100L);
   }
 }
