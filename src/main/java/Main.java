8c312537fc20efb442fc8efd77ff68d4def7463f
yerimkim <kiel0103@naver.com>
2025-01-06 20:48:34 +0900

    예제 10-8 subscribeOn() publishOn()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,18 @@ public class Main {
    *
    * 별도의 비동기 처리(ex: Schedulers)를 명시적으로 추가하지 않았기 때문에, 모든 연산은 순차적으로 실행됩니다.
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Flux
         .fromArray(new Integer[] {1, 3, 5, 7})
+        .subscribeOn(Schedulers.boundedElastic())
         .doOnNext(data -> System.out.printf("# [%s] doOnNext fromArray: %s\n", Thread.currentThread().getName(), data))
-        .publishOn(Schedulers.parallel())
         .filter(data -> data > 3)
         .doOnNext(data -> System.out.printf("# [%s] doOnNext filter: %s\n", Thread.currentThread().getName(), data))
         .publishOn(Schedulers.parallel())
         .map(data -> data * 10)
         .doOnNext(data -> System.out.printf("# [%s] doOnNext map: %s\n", Thread.currentThread().getName(), data))
         .subscribe(data -> System.out.printf("# [%s] onNext: %s\n", Thread.currentThread().getName(), data));
+
+    Thread.sleep(500L);
   }
 }
